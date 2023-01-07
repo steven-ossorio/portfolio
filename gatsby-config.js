80a768d9226e331793880d6f669300ecfe4d3b8d
Steven <codebusiness2014@gmail.com>
2023-01-06 20:00:42 -0600

    working with legacy plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    "gatsby-theme-material-ui",
+    "gatsby-plugin-material-ui",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
